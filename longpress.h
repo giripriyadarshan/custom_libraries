c055ab7c0506026f4a269d821248e0b554874aaa
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2021-04-13 20:29:56 +0530

    first commit

---
+++ longpress.h
@@ -0,0 +1,42 @@
+#ifndef longpress_h
+#define longpress_h
+
+class longpress
+{
+private:
+    unsigned long long_press_trigger;
+    int lpt_c_v = 0; // long_press_trigger control variable
+public:
+    void long_press_millis()
+    {
+        long_press_trigger = millis();
+    }
+
+    void confirm_press()
+    {
+        lpt_c_v = 1;
+    }
+
+    bool trigger(int pin, int long_press_delay, int trigger_state)
+    {
+        if (digitalRead(pin) == trigger_state)
+        {
+            if ((millis() - long_press_trigger) > long_press_delay)
+            {
+                if (lpt_c_v == 1)
+                {
+                    lpt_c_v = 0;
+                    return true;
+                }
+            }
+        }
+
+        // removing false positives
+        if (digitalRead(pin) == HIGH)
+            lpt_c_v = 0;
+
+        return false;
+    }
+};
+
+#endif
